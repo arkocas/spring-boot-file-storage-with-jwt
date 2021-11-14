7a1011564c1dcf9e97391b325fb6a7445db0c7b4
arkocas <cengark@gmail.com>
2021-11-14 17:54:45 +0300

    Initial commit

---
+++ src/main/java/com/alirizakocas/springbootfilestoragewithjwt/SpringBootFileStorageWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.alirizakocas.springbootfilestoragewithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileStorageWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileStorageWithJwtApplication.class, args);
+	}
+
+}
